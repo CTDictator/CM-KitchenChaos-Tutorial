f4c797bfc45088e2aa78ced741b98864c3cb16ec
CTDictator <vladyslavnepo@gmail.com>
2024-01-25 18:37:06 -0500

    Player collisions added
    Using a capsule collider to stop the player from walking through solid objects.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,37 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new(inputVector.x, 0, inputVector.y);
-        transform.position += moveDirection * (Time.deltaTime * moveSpeed);
+        
+        float playerRadius = 0.7f;
+        float playerHeight = 2.0f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, 
+            transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, 
+                transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDirection = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, 
+                    transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDirection = moveDirZ;
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
+
         isWalking = moveDirection != Vector3.zero;
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,moveDirection, Time.deltaTime * rotationSpeed);
