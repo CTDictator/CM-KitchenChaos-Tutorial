697c7f15574ea944a3c276db723c9fe99f90871a
CTDictator <vladyslavnepo@gmail.com>
2024-01-25 18:10:36 -0500

    Refactored player controls
    Changed up the player controls using the new player input system.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,27 +6,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
     private void Update()
     {
-        Vector2 inputVector = new Vector2();
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
-        }
-        inputVector.Normalize();
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new(inputVector.x, 0, inputVector.y);
         transform.position += moveDirection * (Time.deltaTime * moveSpeed);
         isWalking = moveDirection != Vector3.zero;
